75ccb36755ca1263b9f452f00271bce5fdef3285
PickleBoxer <matic.vertacnik@icloud.com>
2023-12-20 10:20:56 +0100

    Add Roboto font to AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,6 +35,7 @@ class AdminPanelProvider extends PanelProvider
                 'grey' => Color::Zinc,
 
             ])
+            ->font('roboto')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
